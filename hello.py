32cf688f6a804e8f5e85f6b8764d2e75a5b749d9
israelcvb <israelcvb21@gmail.com>
2023-06-08 14:28:11 -0300

    Redirecionamento e sessões de usuário.

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -11,7 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Eu e a Mucura na canoa!'
 
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 
 class NameForm(FlaskForm):
@@ -21,24 +17,8 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
